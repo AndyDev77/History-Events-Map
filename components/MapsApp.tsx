c6f701d3b5011ef25c1cf753d15ed375280fa75f
AndyDev77 <andyvespuce77550@gmail.com>
2024-01-17 00:41:28 +0100

    first commit

---
+++ components/MapsApp.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+
+const defaultPosition: [number, number] = [51.505, -0.09];
+
+const MapsApp = () => {
+  return (
+    <main className="content">
+      <div className="flex flex-col w-full h-full">
+        <div className="h-12"></div>
+        <MapContainer
+          center={defaultPosition}
+          zoom={13}
+          className="map-container"
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={defaultPosition}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </main>
+  );
+};
+
+export default MapsApp;
